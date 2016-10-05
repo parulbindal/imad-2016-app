d5dbe88215fae46015a73c81373cf5ef4e42b0b8
parulbindal <parulbindal96@gmail.com>
2016-10-05 21:38:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
 var img = document.getElementById('madi');
-
+var marginLeft = 0;
+function moveRight(){
+    marginLeft += 1;
+    img.style.marginLeft = marginleft + 'px';
+}
 img.onclick = function(){
-  img.style.marginLeft = '100px';  
+    var interval = setInterval(moveRight ,50);
+ 
 }; 
+
