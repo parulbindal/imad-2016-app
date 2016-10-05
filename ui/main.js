512f84ed956f8494951b53fc3631052c22babcdd
parulbindal <parulbindal96@gmail.com>
2016-10-05 21:39:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML = 'new value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft += 1;
+    marginLeft =  marginLeft + 1;
     img.style.marginLeft = marginleft + 'px';
 }
 img.onclick = function(){
