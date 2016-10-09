8f004ad9180cfbac006d7f6b2e771378d12d61c4
parulbindal <parulbindal96@gmail.com>
2016-10-09 08:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function(){
   var names = {"names1" , "names2" , "names3"};
   var list = '';
   for(var i = 0 ; i<names.length ;i++){
-      list += '<li>' + names[i] + '</li>'
+      list += '<li>' + names[i] + '</li>';
   }
   var ul = document.getElementById("namelist");
   ul.innerHTML = list;
