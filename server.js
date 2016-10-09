75f35f8f3944afa0b2dd1c7801ac4676d52cbf02
parulbindal <parulbindal96@gmail.com>
2016-10-09 10:47:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,9 +112,9 @@ app.get('/ui/madi.png', function (req, res) {
 // });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){
    //get the names from the request
-   var name = req.params.name;
+   var name = req.query.name;
    
    names.push(name);
    //JSON: javascript Object Notation
