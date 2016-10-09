ebf293cac55271a972582bd50015f640efe53255
parulbindal <parulbindal96@gmail.com>
2016-10-09 08:41:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,21 @@ button.onclick = function(){
   request.send(null); 
   
 };
+
+//Submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+  //Make a request to the server and send the name
+  
+  
+  //Capture a list of names and render it as a list
+  var names = {"names1" , "names2" , "names3"};
+  var list = '';
+  for(var i = 0 ; i<names.length ;i++){
+      list += '<li>' + names[i] + '</li>'
+  }
+  var ul = document.getElementById("namelist");
+  ul.innerHTML = list;
+};
